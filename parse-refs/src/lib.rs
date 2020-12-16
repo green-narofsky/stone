80393007d0fba9c63075f3515f9d98005e050b51
John Matthew Narofsky <7ytd765789@gmail.com>
2020-12-16 11:09:09 -0600

    move tests over

--- parse-refs/src/lib.rs
+++ parse-refs/src/lib.rs
@@ -275,4 +275,27 @@ mod tests {
         let b = parse_ref!(&*a, Lol);
         let c = &b[1];
     }
+    #[test]
+    fn slice_offsets() {
+        let a = b"Hello, world!";
+        let b: &Tagged<[u8], _> = tag_ref!(a);
+        let c = &b[1..];
+        let d = &b[4..];
+        assert_eq!(d.offset_from(c), 3);
+    }
+    #[test]
+    fn vec_offsets() {
+        let a = vec![1, 2, 3];
+        let b = tag_ref!(&*a);
+        let c = &b[1..2];
+        assert_eq!(c.offset_from(b), 1);
+    }
+    #[test]
+    fn item_offsets() {
+        let a = b"Hello, world!";
+        let b: &Tagged<[u8], _> = tag_ref!(a);
+        let c = &b[1];
+        let d = &b[4];
+        assert_eq!(d.offset_from(c), 3);
+    }
 }
--- parse-refs/src/string.rs
+++ parse-refs/src/string.rs
@@ -2,34 +2,5 @@
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    #[test]
-    fn slice_offsets() {
-        let a = b"Hello, world!";
-        let b: &Tagged<[u8], _> = tag_ref!(a);
-        let c = &b[1..];
-        let d = &b[4..];
-        assert_eq!(d.offset_from(c), 3);
-    }
-    #[test]
-    fn vec_offsets() {
-        let a = vec![1, 2, 3];
-        let b = tag_ref!(&*a);
-        let c = &b[1..2];
-        assert_eq!(c.offset_from(b), 1);
-    }
-    #[test]
-    fn indexing() {
-        let a = vec![1, 2, 3];
-        let b = tag_ref!(&*a);
-        let c = &b[1];
-    }
-    #[test]
-    fn item_offsets() {
-        let a = b"Hello, world!";
-        let b: &Tagged<[u8], _> = tag_ref!(a);
-        let c = &b[1];
-        let d = &b[4];
-        assert_eq!(d.offset_from(c), 3);
-    }
+
 }
